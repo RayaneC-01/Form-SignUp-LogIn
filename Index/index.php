ade607087c4ccfd602aadd31e11ba6e2b0cf11e6
Rayane <rchaabane20@gmail.com>
2024-02-22 22:38:26 +0100

    maj dossier Index

--- Index/index.php
+++ Index/index.php
@@ -1,3 +1,4 @@
+<? require 'login.php' ?>
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -6,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Page de Connexion</title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Bootstrap icons (optional) -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="/styles/style.css" />
@@ -15,7 +16,7 @@
 
 <body>
     <div class="container">
-        <form class="login-form" method="POST" action="login.php">
+        <form class="login-form" method="POST" action="accueil.php">
             <h2>Connexion</h2>
             <input type="text" id="email" name="email" placeholder="username ou email" required />
             <div class="password-input">
@@ -29,8 +30,9 @@
             <?php echo isset($message_succes) ? '<p class="p-3 mb-2 bg-success text-white">' . $message_succes . '</p>' : ''; ?>
         </form>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
     </script>
     <script src="/Script/script.js"></script>
 </body>
--- Index/inscription.php
+++ Index/inscription.php
@@ -1,3 +1,4 @@
+<? require 'inscription_process.php' ?>
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -38,7 +39,7 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+        </script>
     <script src="/Script/script.js"></script>
 </body>
 
